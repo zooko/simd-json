26a671f60228123cb5b6dd1a8da136dff6523244
Zsomething <nothanks@ontheemail.com>
2026-01-04 09:27:33 -0700

    add script to bench all the allocators and compare their performance

---
+++ bench-allocators.sh
@@ -0,0 +1,11 @@
+echo CPU type:
+
+# print out CPU type on macos
+sysctl -n machdep.cpu.brand_string 2>/dev/null
+
+# print out CPU type on linux
+grep "model name" /proc/cpuinfo | uniq 2>/dev/null
+
+cargo bench 2>&1 | tee default
+for AL in jemalloc mimalloc rpmalloc snmalloc smalloc; do BLNAME=${AL}; cargo bench --features=${AL} 2>&1 | tee ${BLNAME} ; done
+./critcmp.py default jemalloc mimalloc rpmalloc snmalloc smalloc
