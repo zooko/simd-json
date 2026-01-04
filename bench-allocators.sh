86bd18d954f90129d554d226b154cdc927032a0f
Zsomething <nothanks@ontheemail.com>
2026-01-04 16:57:14 -0700

    quiet error messages when run on non-linux

--- bench-allocators.sh
+++ bench-allocators.sh
@@ -4,7 +4,7 @@ echo CPU type:
 sysctl -n machdep.cpu.brand_string 2>/dev/null
 
 # print out CPU type on linux
-grep "model name" /proc/cpuinfo | uniq 2>/dev/null
+grep "model name" /proc/cpuinfo 2>/dev/null | uniq
 
 cargo bench 2>&1 | tee default
 for AL in jemalloc mimalloc rpmalloc snmalloc smalloc; do BLNAME=${AL}; cargo bench --features=${AL} 2>&1 | tee ${BLNAME} ; done
