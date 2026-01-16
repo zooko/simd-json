1ff129648d0de802572faab5f15fa9339fdf333f
Zsomething <nothanks@ontheemail.com>
2026-01-16 14:24:22 -0700

    add git dirty/clean flag to results file, skip jemalloc on Windows

--- bench-allocators.sh
+++ bench-allocators.sh
@@ -10,24 +10,42 @@ CPUTYPE="${CPUTYPE//[^[:alnum:]]/}"
 
 OSTYPESTR="${OSTYPE//[^[:alnum:]]/}"
 
-RESF=simd-json.bench-allocators.result.${CPUTYPE}.${OSTYPESTR}.txt
+ARGS=$*
+ARGSSTR="${ARGS//[^[:alnum:]]/}"
 
-echo "# Saving result into a file named \"${RESF}\" ..."
+BNAME="simd-json"
+FNAME="${BNAME}.result.${CPUTYPE}.${OSTYPESTR}.${ARGSSTR}.txt"
+TMPF="tmp/${FNAME}"
+RESF="${FNAME}"
 
-rm -f $RESF
+echo "# Saving result into a tmp file (in ./tmp) which will be moved to \"${RESF}\" when complete..."
 
-echo "# git log -1 | head -1" 2>&1 | tee -a $RESF
-git log -1 | head -1 2>&1 | tee -a $RESF
-echo 2>&1 | tee -a $RESF
+rm -f $TMPF
+mkdir -p tmp
 
-echo CPU type: 2>&1 | tee -a $RESF
-echo $CPUTYPE 2>&1 | tee -a $RESF
-echo 2>&1 | tee -a $RESF
+echo "# git log -1 | head -1" 2>&1 | tee -a $TMPF
+git log -1 | head -1 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
 
-echo OS type: 2>&1 | tee -a $RESF
-echo $OSTYPE 2>&1 | tee -a $RESF
-echo 2>&1 | tee -a $RESF
+echo "( [ -z \"\$(git status --porcelain)\" ] && echo \"Clean\" || echo \"Uncommitted changes\" )" 2>&1 | tee -a $TMPF
+( [ -z "$(git status --porcelain)" ] && echo "Clean" || echo "Uncommitted changes" ) 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
+
+echo CPU type: 2>&1 | tee -a $TMPF
+echo $CPUTYPE 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
+
+echo OS type: 2>&1 | tee -a $TMPF
+echo $OSTYPE 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
+
+if [ "x${OSTYPE}" = "xmsys" ]; then
+	# no jemalloc on windows
+	ALLOCATORS="mimalloc rpmalloc snmalloc smalloc"
+else
+	ALLOCATORS="mimalloc rpmalloc jemalloc snmalloc smalloc"
+fi
 
 cargo --locked bench 2>&1 | tee default
-for AL in jemalloc mimalloc rpmalloc snmalloc smalloc; do BLNAME=${AL}; cargo --locked bench --features=${AL} 2>&1 | tee ${BLNAME} ; done
-./critcmp.py default jemalloc mimalloc rpmalloc snmalloc smalloc 2>&1 | tee -a $RESF
+for AL in ${ALLOCATORS} ; do BLNAME=${AL}; cargo --locked bench --features=${AL} 2>&1 | tee ${BLNAME} ; done
+./critcmp.py default ${ALLOCATORS} 2>&1 | tee -a $RESF
