2a6ccf469f72d42ae65f57287e11ae8eb100591a
Nothanks Onthelegiblenames <nothanks@ontheemail.com>
2026-01-16 01:10:21 +0000

    print out more details about CPU and operating system, depend on smalloc >= v7

--- bench-allocators.sh
+++ bench-allocators.sh
@@ -1,10 +1,20 @@
+# CPU type on linuxy
+CPUTYPE=`grep "model name" /proc/cpuinfo 2>/dev/null | uniq | cut -d':' -f2-`
+
+if [ "x${CPUTYPE}" = "x" ] ; then
+    # CPU type on macos
+    CPUTYPE=`sysctl -n machdep.cpu.brand_string 2>/dev/null`
+fi
+
+CPUTYPE="${CPUTYPE//[^[:alnum:]]/}"
+
 echo CPU type:
+echo $CPUTYPE
+echo
 
-# print out CPU type on macos
-sysctl -n machdep.cpu.brand_string 2>/dev/null
-
-# print out CPU type on linux
-grep "model name" /proc/cpuinfo 2>/dev/null | uniq
+echo OS type:
+echo $OSTYPE
+echo
 
 cargo bench 2>&1 | tee default
 for AL in jemalloc mimalloc rpmalloc snmalloc smalloc; do BLNAME=${AL}; cargo bench --features=${AL} 2>&1 | tee ${BLNAME} ; done
