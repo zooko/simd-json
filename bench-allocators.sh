aab8ca6365c0952f9e0dde89421cd5abd3b56516
Zsomething <nothanks@ontheemail.com>
2026-01-23 17:08:13 -0700

    tidy up script and make it more similar to the comparable scripts in rebar, smalloc, and bench-allocators

--- bench-allocators.sh
+++ bench-allocators.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+set -e
+
 BNAME="simd-json"
 
 # Collect metadata
-GITCOMMIT="$(git log -1 | head -1 | cut -d' ' -f2)"
+GITCOMMIT=$(git rev-parse HEAD)
 GITCLEANSTATUS=$( [ -z "$( git status --porcelain )" ] && echo \"Clean\" || echo \"Uncommitted changes\" )
 TIMESTAMP=$(date -u +"%Y-%m-%d %H:%M:%S UTC")
 
 # CPU type on linuxy
-CPUTYPE=`grep "model name" /proc/cpuinfo 2>/dev/null | uniq | cut -d':' -f2-`
-if [ "x${CPUTYPE}" = "x" ] ; then
+CPUTYPE=$(grep -m1 "model name" /proc/cpuinfo 2>/dev/null | cut -d':' -f2-)
+if [ -z "${CPUTYPE}" ] ; then
     # CPU type on macos
-    CPUTYPE=`sysctl -n machdep.cpu.brand_string 2>/dev/null`
+    CPUTYPE=$(sysctl -n machdep.cpu.brand_string 2>/dev/null || echo "Unknown")
 fi
 CPUTYPESTR="${CPUTYPE//[^[:alnum:]]/}"
 OSTYPESTR="${OSTYPE//[^[:alnum:]]/}"
@@ -22,12 +24,16 @@ OUTPUT_DIR="${OUTPUT_DIR:-./benchmark-results}/${CPUSTR_DOT_OSSTR}"
 RESF="${OUTPUT_DIR}/${BNAME}.result.txt"
 GRAPHF="${OUTPUT_DIR}/${BNAME}.graph.svg"
 
+mkdir -p tmp
+rm -f $RESF $GRAPHF
+
+echo "GITCOMMIT: ${GITCOMMIT}" 2>&1 | tee -a $RESF
+echo "GITCLEANSTATUS: ${GITCLEANSTATUS}" 2>&1 | tee -a $RESF
+echo "CPUTYPE: ${CPUTYPE}" 2>&1 | tee -a $RESF
+echo "OSTYPE: ${OSTYPE}" 2>&1 | tee -a $RESF
+
 mkdir -p ${OUTPUT_DIR}
 
-echo "# Saving result into \"${RESF}\""
-echo "# Saving graph into \"${GRAPHF}\""
-rm -f $RESF $GRAPHF
-mkdir -p tmp
 
 if [ "x${OSTYPE}" = "xmsys" ]; then
     # no jemalloc or snmalloc on windows
@@ -38,7 +44,6 @@ fi
 
 TMPALLOS="tmp/${ALLOCATORS// / tmp/}"
 
-set -e
 # Run benchmarks
 cargo --locked bench 2>&1 | tee tmp/default
 for AL in ${ALLOCATORS} ; do 
